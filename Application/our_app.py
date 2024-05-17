b311394245203eb098766fbba9dcca89b2faf909
S0nia11 <146825870+S0nia11@users.noreply.github.com>
2024-05-17 23:36:45 +0200

    Structure du travail fait

--- Application/our_app.py
+++ Application/our_app.py
@@ -1,16 +1,41 @@
 import streamlit as st
+import os
 
 def accueil():
     st.title('Page d\'accueil')
     st.write('Bienvenue dans notre application !')
+    try:
+        image_path = os.path.join(os.path.dirname(__file__), 'tennis.jpg')
+        if os.path.exists(image_path):
+            st.image(image_path, use_column_width=True)
+        else:
+            st.error(f"Image non trouvée : {image_path}")
+    except Exception as e:
+        st.error(f"Erreur lors du chargement de l'image : {e}")
 
 def graphiques():
     st.title('Page des graphiques')
-    st.write('Ici, vous verrez des graphiques sur les performances des joueurs. Les graphiques seront ajoutés ultérieurement.')
+    st.write('Ici, vous verrez des graphiques réalisés.')
+    try:
+        image_path = os.path.join(os.path.dirname(__file__), 'OIP.jpg')
+        if os.path.exists(image_path):
+            st.image(image_path, use_column_width=True)
+        else:
+            st.error(f"Image non trouvée : {image_path}")
+    except Exception as e:
+        st.error(f"Erreur lors du chargement de l'image : {e}")
 
 def modeles_ml():
     st.title('Page des modèles de ML')
-    st.write('Ici, vous verrez les modèles de prédiction et leurs résultats. Les modèles seront ajoutés ultérieurement.')
+    st.write('Ici, vous verrez les modèles réalisés et leurs résultats.')
+    try:
+        image_path = os.path.join(os.path.dirname(__file__), 'img.jpg')
+        if os.path.exists(image_path):
+            st.image(image_path, use_column_width=True)
+        else:
+            st.error(f"Image non trouvée : {image_path}")
+    except Exception as e:
+        st.error(f"Erreur lors du chargement de l'image : {e}")
 
 def main():
     st.sidebar.title('Menu')
