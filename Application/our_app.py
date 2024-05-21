fb2ada5c42d08d7115ee06d2282d81acf3764f03
S0nia11 <146825870+S0nia11@users.noreply.github.com>
2024-05-21 04:20:35 +0200

    m

--- Application/our_app.py
+++ Application/our_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import os
 import base64
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
@@ -11,12 +14,11 @@ def add_bg_from_local(image_file):
         .stApp {{
             background-image: url(data:image/{"png"};base64,{encoded_string});
             background-size: cover;
-        }}
-        .white-text {{
             color: white;
         }}
         .content {{
             position: relative;
+            color: white;
         }}
         .images {{
             text-align: center;
@@ -58,9 +60,125 @@ def accueil():
 
 def graphiques():
     add_bg_from_local(os.path.join('static', 'wt.webp'))
-    st.markdown('<h1 class="white-text">Page des graphiques</h1>', unsafe_allow_html=True)
-    st.markdown('<p class="white-text">Ici, vous verrez des graphiques réalisés.</p>', unsafe_allow_html=True)
+    st.markdown('<h1 class="white-text">Tennis : Data Storytelling</h1>', unsafe_allow_html=True)
+    
+    # Intro
+    st.header("Introduction")
+    st.subheader("1. Histoire")
+    st.markdown("""
+    Le tennis, descendant du jeu de paume français du 11e siècle, impliquait de frapper une balle avec la paume de main pour la faire rebondir contre les murs et les pans inclinés. Au 19e siècle, en Angleterre, le tennis moderne a pris son essor grâce au All England Croquet Club, qui a ouvert ses terrains de croquet à ce nouveau sport. Cette décision a marqué le début de la popularité du sport. Depuis, de nombreuses fédérations nationales ont été créées dans le monde entier afin de régir ce sport en pleine expansion.
+    """, unsafe_allow_html=True)
 
+    st.subheader("2. Jeux Olympiques 2024")
+    st.markdown("""
+    Aux JO, le tennis présente des épreuves masculines et féminines de simple et de double, ainsi qu'une épreuve de double mixte. Les matchs individuels pour hommes et femmes se jouent au meilleur des trois sets, avec l'introduction d'un tie-break dans le set décisif de chaque discipline. Cependant, dans le double mixte, en cas d'égalité à un set partout, un super tie-break est disputé pour départager les joueurs.
+    """, unsafe_allow_html=True)
+    
+    st.subheader("3. Mots clés")
+    st.markdown("""
+    - épreuve simple/double/double mixte : Les joueurs se s’affrontent en tête-à-tête/deux équipes de deux s’affrontent/deux équipes composées d’un homme et d’une femme.  
+    - Hard/Clay/Grass/Carpet court : Terrain dur, en terre battue, de gazon, en moquette.
+    - GOAT : Greatest of All Time, soit le plus grand de tous les temps.
+    - Set : Série de jeux disputés, le premier qui atteint un certain nombre de jeux remporte le set.
+    - Rank, Seed : Classement.
+    - Runner-up : Finaliste.
+    - Major : Les quatres tournoi du Grand Chelem: L’Open Australie, France, Wimbledon(London) et les US.
+    - Aces : Des services qui ne sont pas retournés par l'adversaire et qui permettent au serveur de marquer directement un point.
+    """, unsafe_allow_html=True)
+    
+    # Problématique
+    st.header("Problématiques")
+    st.markdown("""
+    1. Les facteurs influençant les performances des joueurs (physiques,...)
+    2. Les facteurs causant des blessures aux joueurs
+    """, unsafe_allow_html=True)
+    
+    # Objectifs
+    st.header("Objectifs")
+    st.markdown("""
+    - Optimisation des performances des joueurs aux Jeux Olympiques 2024.
+    - Prédiction des blessures.
+    """, unsafe_allow_html=True)
+    
+    # Plan 
+    st.header("Plan suivi")
+    st.subheader("Collecte des données")
+    st.markdown("""
+    **1.1 Données collectées :**
+    - Tournois : Année, Champions, Finalistes, Scores, Points, Classements, Surface.
+    - Joueurs : Nom, Âge, Pays, Taille, Poids, Main Utilisée.
+    - Terrains : Nom, Adresse, Ville, Surface, Latitude, Longitude, Lumière, Couvert.
+    - Équipements : Nom Complexe, Type, Utilisation, Utilisateurs, Année de mise en service, Type d’énergie utilisée.
+    - Blessures : Nom, Type, Endroit du corps, Cause, Surface, Traitement.
+    
+    **1.2 Moyens utilisés :**
+    - Open data, Scrapping, OpenStreetMap (données cartographiques)
+    """, unsafe_allow_html=True)
+    
+    st.image('static/eq.png', use_column_width=True)
+    
+    # Analyse des données
+    st.header("3. Analyse des données")
+    
+    
+    st.markdown("""
+    1. Les evolutions :
+    """)
+    st.image('static/tournois_temps.png', use_column_width=True)
+    st.image('static/Tournois_surfaces.png', use_column_width=True)
+    st.image('static/classement_HFF.png', use_column_width=True)
+    
+    st.markdown("""
+    2. Terrains / Equipements :
+    """)
+    st.image('static/etr.png', use_column_width=True)
+    
+    
+    df_fr = pd.read_excel('fr-en-dataes-types-d-equipement_nettoye.xlsx')
+    equipements_uniques = df_fr['EquipementTypeLib'].unique()
+    non_tennis_equipements = [equipement for equipement in equipements_uniques if 'tennis' not in str(equipement).lower()]
+    df_tennis_cleaned = df_fr[~df_fr['EquipementTypeLib'].isin(non_tennis_equipements)]
+    
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+    installations_par_niveau_type = df_tennis_cleaned.groupby(['EquipementTypeLib', 'Niveau pratiqué'])['InsNom'].count().unstack()
+    installations_par_niveau_type.plot(kind='bar', stacked=True, ax=axes[0])
+    axes[0].set_xlabel('Type d\'équipement')
+    axes[0].set_ylabel('Nombre d\'installations')
+    axes[0].set_title('Niveau pratiqué par type d\'équipement')
+    axes[0].legend(title='Niveau pratiqué')
+    axes[0].tick_params(axis='x', rotation=90)
+    axes[0].grid(True)
+    nature_par_type = df_tennis_cleaned.groupby(['EquipementTypeLib', 'NatureLibelle']).size().unstack()
+    nature_par_type.plot(kind='bar', stacked=True, ax=axes[1])
+    axes[1].set_xlabel('Type d\'équipement')
+    axes[1].set_ylabel('Nombre d\'installations')
+    axes[1].set_title('Répartition des installations par nature et type d\'équipement')
+    axes[1].legend(title='Nature')
+    axes[1].tick_params(axis='x', rotation=90)
+    axes[1].grid(True)
+    plt.tight_layout()
+    
+    # Affichage du graphe
+    st.pyplot(fig)
+    
+    st.image('static/instsport.png', use_column_width=True)
+    st.image('static/util_terr.png', use_column_width=True)
+    st.image('static/balles.png', use_column_width=True)
+    
+    
+    st.markdown("""
+    3. Joueurs :
+    """)
+    st.image('static/pays.png', use_column_width=True)
+    st.image('static/clssatp.png', use_column_width=True)
+    st.image('static/clss_fm.png', use_column_width=True)
+    st.image('static/Joueurs.png', use_column_width=True)
+    st.image('static/aces.png', use_column_width=True)
+    st.image('static/main.png', use_column_width=True)
+    
+    
+    
+    
 def modeles_ml():
     add_bg_from_local(os.path.join('static', 'img.jpg'))
     st.markdown('<h1 class="white-text">Page des modèles de ML</h1>', unsafe_allow_html=True)
@@ -72,7 +190,7 @@ def main():
 
     if page == 'Accueil':
         accueil()
-    elif page == 'Graphiques':
+    elif page =='Graphiques':
         graphiques()
     elif page == 'Modèles':
         modeles_ml()
